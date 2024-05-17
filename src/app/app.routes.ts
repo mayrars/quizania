ca94790a6bdaa9cbcc4fff4730b2b45145605524
Mayra <mayra203@gmail.com>
2024-05-17 06:53:50 -0600

    created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InitComponent } from './pages/init/init.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [    
+    { path: '', component: HomeComponent },
+    { path: 'init', component: InitComponent },
+];
