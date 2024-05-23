8a2a4d06593cc5904922b356d1cdc3b33f1bf561
Mayra <mayra203@gmail.com>
2024-05-23 06:59:44 -0600

    change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { InitComponent } from './pages/init/init.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 export const routes: Routes = [    
     { path: '', component: HomeComponent },
-    { path: 'init', component: InitComponent },
+    { path: 'category/:id', component: CategoryComponent },
 ];
