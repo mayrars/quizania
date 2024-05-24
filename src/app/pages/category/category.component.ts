d853e5481659609eee3b2e30b6a517f21894bad9
Mayra <mayra203@gmail.com>
2024-05-24 06:59:14 -0600

    modify category

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { ActivatedRoute,Params } from '@angular/router';
 
 @Component({
   selector: 'app-category',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
-export class CategoryComponent {
-
+export class CategoryComponent implements OnInit{
+  private _apiService = inject(ApiService);
+  private _route = inject(ActivatedRoute)
+  category!: number;
+  questions: any[] = [];
+  numberOfQuestions: number = 0;
+  constructor(private rutaActiva: ActivatedRoute) { }
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      console.log(params)
+      this.category = params['id'];
+      this._apiService.getQuestions(this.category).subscribe(data=>{
+        this.numberOfQuestions = data.results.length;
+      })
+    })
+  }
 }
