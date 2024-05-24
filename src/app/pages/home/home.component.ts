af5bd039729247436741134727bf5ad64e3a5aa2
Mayra <mayra203@gmail.com>
2024-05-24 06:58:51 -0600

    remove variables

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,6 @@ import { afterNextRender, Component, ElementRef, inject, OnInit, ViewChild } fro
 import { RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { ICategories } from '../../models/categories.model';
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -13,9 +12,6 @@ import { ICategories } from '../../models/categories.model';
 export class HomeComponent implements OnInit {
   private _apiService = inject(ApiService);
   categories: ICategories[] = [];
-  colors: string[] = ['blue', 'red', 'green', 'yellow', 'orange', 'purple', 'pink', 'esmerald', 'sky', 'indigo'];
-  range: string[] =["200", "300", "400", "500", "600", "700", "800", "900"];
- 
   ngOnInit(): void {    
     this._apiService.getCategories().subscribe(res=> {
       this.categories = res.trivia_categories.map((item: any) => {
