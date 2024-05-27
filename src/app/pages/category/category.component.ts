348b1472651f22df90437825f22bcd8724677ed2
Mayra <mayra203@gmail.com>
2024-05-27 07:00:55 -0600

    change category section

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
-import { ActivatedRoute,Params } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { StepperComponent } from '../../components/stepper/stepper.component';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [StepperComponent],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
@@ -20,6 +21,7 @@ export class CategoryComponent implements OnInit{
     this._route.params.subscribe(params => {
       this.category = params['id'];
       this._apiService.getQuestions(this.category).subscribe(data=>{
+        this.questions = data.results
         this.numberOfQuestions = data.results.length;
       })
     })
