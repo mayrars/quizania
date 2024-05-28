4986c414ac6f8611c0f7ab99482f37f5bd9b38ff
Mayra <mayra203@gmail.com>
2024-05-28 06:57:36 -0600

    created form in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,16 +2,25 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { ICategories } from '../../models/categories.model';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule, NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+  initForm!: FormGroup
   private _apiService = inject(ApiService);
   categories: ICategories[] = [];
+  constructor(private formBuilder: FormBuilder){
+    this.initForm = this.formBuilder.group({
+      levelQuestions: ['',[Validators.required]],
+      typeQuestions: ['',[Validators.required]],
+    })
+  }
   ngOnInit(): void {    
     this._apiService.getCategories().subscribe(res=> {
       this.categories = res.trivia_categories.map((item: any) => {
@@ -27,4 +36,8 @@ export class HomeComponent implements OnInit {
   getRandomNumber(min:number, max:number) {
     return Math.random() * (max - min) + min
   }
+  hasErrors(controlName: string,errorType: string){
+    return this.initForm.get(controlName)?.hasError(errorType) && this.initForm.get(controlName)?.touched
+  }
+  onSubmit(){}
 }
