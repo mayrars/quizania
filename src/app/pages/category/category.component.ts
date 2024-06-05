327e4d9b71d8ff883f0982b57a60a7fc8061e10c
Mayra <mayra203@gmail.com>
2024-06-05 07:01:41 -0600

    adding category buttons

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -15,16 +15,37 @@ export class CategoryComponent implements OnInit{
   private _apiService = inject(ApiService);
   private _route = inject(ActivatedRoute)
   category!: number;
+  typeQuestions!: string | null;
+  levelQuestions!: string | null;
+  currentQuestion: number = 0;
   questions: any[] = [];
   numberOfQuestions: number = 0;
   constructor(private rutaActiva: ActivatedRoute) { }
   ngOnInit(): void {
     this._route.params.subscribe(params => {
       this.category = params['id'];
-      this._apiService.getQuestions(this.category).subscribe(data=>{
+      this.levelQuestions = this._route.snapshot.queryParamMap.get('level') || 'easy' 
+      this.typeQuestions = this._route.snapshot.queryParamMap.get('type') || 'multiple' 
+      this._apiService.getQuestions(this.category,10,this.levelQuestions,this.typeQuestions).subscribe(data=>{
         this.questions = data.results
         this.numberOfQuestions = data.results.length;
+        console.log(this.numberOfQuestions)
       })
     })
+  }
+  //Go to next question
+  nextQuestion(){
+    if(this.currentQuestion < this.numberOfQuestions-1){
+      this.currentQuestion++;
+    }
+    console.log(this.currentQuestion)
+  }
+  //Go to previous question
+  previousQuestion(){
+    console.log(this.numberOfQuestions)
+    if(this.currentQuestion > 0){
+      this.currentQuestion--;
+    }
+    console.log(this.currentQuestion)
   }
 }
